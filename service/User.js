b6fb1ec4e3140e257dcd89b350aba53c3b2e27eb
Junior Schmidt <junior.schmidt.99@gmail.com>
2022-05-31 19:57:04 -0300

    Update User.js

--- service/User.js
+++ service/User.js
@@ -35,15 +35,68 @@ class User{
     }
     async findById(id){
         try{
-            var query = await database.select(['id', 'name', 'email', 'role']).where({id:id}).table('api_users')
-            if(query.length > 0){
-                return query[0]
+            var result = await database.select(["id","email","role","name"]).where({id:id}).table("api_users");
+            var resultArray = Object.values(JSON.parse(JSON.stringify(result)))
+            if(resultArray.length > 0){
+                return resultArray[0];
             }else{
                 return undefined;
             }
+
         }catch(err){
             console.log(err);
             return undefined;
+        }
+    }
+    async update(id, email, name, role){
+
+        var user = await this.findById(id);
+        console.log(user)
+        if(user != undefined){
+
+            var editUser = {};
+
+            if(email != undefined){ 
+                if(email != user.email){
+                   var result = await this.findEmail(email);
+                   if(result == false){
+                        editUser.email = email;
+                   }else{
+                        return {status: false,err: "Email is already registered"}
+                   }
+                }
+            }
+
+            if(name != undefined){
+                editUser.name = name;
+            }
+
+            if(role != undefined){
+                editUser.role = role;
+            }
+
+            try{
+                await database.update(editUser).where({id: id}).table("api_users");
+                return {status: true}
+            }catch(err){
+                return {status: false,err: err}
+            }
+            
+        }else{
+            return {status: false,err: "The user does not exist!"}
+        }
+    }
+    async delete(id){
+        var user = await this.findById(id);
+        if(user != undefined){
+            try{
+                await database.delete().where({id: id}).table("api_users");
+                return {status:true}
+            }catch(err){
+                return{status: false,err:err}
+            }
+        }else{
+            return{status: false,err:'Error'}
         }
     }
 }

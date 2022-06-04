7491ebf68be4eb055c969b5c9c7bbb30988d9c54
Junior Schmidt <junior.schmidt.99@gmail.com>
2022-06-04 19:30:04 -0300

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -4,13 +4,14 @@ var app = express();
 var router = express.Router();
 var HomeController = require('../controllers/HomeController');
 var UserController = require('../controllers/UserController');
+var AdminAuth = require('../middleware/AdminAuth');
 
 router.get('/', HomeController.index);
 router.post('/user', UserController.create);
-router.get('/user', UserController.index);
-router.get('/user/:id', UserController.findUser);
-router.delete('/user/:id', UserController.remove);
-router.put('/user/:id', UserController.editUser);
+router.get('/user', AdminAuth, UserController.index);
+router.get('/user/:id', AdminAuth, UserController.findUser);
+router.delete('/user/:id', AdminAuth, UserController.remove);
+router.put('/user/:id', AdminAuth, UserController.editUser);
 router.post('/recoverPassword', UserController.recoverPassword);
 router.post('/changePassword', UserController.changePassword);
 router.post('/login', UserController.login);
